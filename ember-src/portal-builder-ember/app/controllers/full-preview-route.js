537bb7789a7574172d8bd2e18b86003dfd87af2e
Mudhabir <mudhabir.ahmed@freshworks.com>
2019-11-28 13:48:30 +0530

    Loop through sidebar section data

--- ember-src/portal-builder-ember/app/controllers/full-preview-route.js
+++ ember-src/portal-builder-ember/app/controllers/full-preview-route.js
@@ -1,11 +1,32 @@
 import Controller from '@ember/controller';
-import { get, set } from '@ember/object';
+import { computed, get, set } from '@ember/object';
+import { inject as service } from '@ember/service';
 
+const SIDEBAR_SECTION = {
+  portalHome: "Portal home",
+  solutionsHome: "Solutions home",
+  discussionsHome: "Discussions home",
+  discussions: "Discussions",
+  newTicket: "New ticket",
+  ticketList: "Ticket list",
+  ticketDetails: "Ticket details",
+  topicList: "Topic list"
+}
 export default Controller.extend({
+  data: service('portal-data'),
   showSidebar: true,
+  currentPage: 'html',
+  sidebarSection: computed({
+    get() {
+      return SIDEBAR_SECTION;
+    }
+  }),
   actions: {
     toggleSidebar() {
       set(this, 'showSidebar', !get(this, 'showSidebar'))
+    },
+    transitionToCurrentPage(currentPage) {
+      set(this, 'currentPage', currentPage);
     }
   }
 });
