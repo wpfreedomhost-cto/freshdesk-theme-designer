24a7ac670f58642382185eae1de96b575da642e3
Robin Philip Thomas <robint@Robin-mac.local>
2019-11-28 23:08:57 +0530

    adding cardlist

--- ember-src/portal-builder-ember/app/portal-component-generators/card-list.js
+++ ember-src/portal-builder-ember/app/portal-component-generators/card-list.js
@@ -3,47 +3,36 @@ export const cardList = () => {
     htmlString: '',
 
     constructLiquidString: ({
+      title,
+      listName
     }) => {
       return `
         <div class="container">
           <div class="flex flex-col">
           <div class="flex items-center justify-center pt-10">
-            <span class="text-4xl font-bold">header text</div>
+            <span class="text-4xl font-bold">${title}</div>
           </div> 
           
         <div class="container mx-auto p-8">
           <div class="flex flex-row flex-wrap -mx-2">       
-            <div class="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
-              <div class="relative bg-white rounded border">
-                <div class="p-4">
-                  <h3 class="text-lg font-bold">
-                    <a class="stretched-link" href="#" title="Card 1">
-                      Card 1
-                    </a>
-                  </h3>
-                  <time class="block mb-2 text-sm text-gray-600" datetime="2019-01-01">1st January 2019</time>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  </p>
+            {% for result in ${listName} %}
+              <div class="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
+                <div class="relative bg-white rounded border">
+                  <div class="p-4">
+                    <h3 class="text-lg font-bold">
+                      <a class="stretched-link" href="#" title="Card 1">
+                        {{ result.name }}
+                      </a>
+                    </h3>
+                    <time class="block mb-2 text-sm text-gray-600" datetime="2019-01-01">1st January 2019</time>
+                    <p>
+                      {{ result.description }}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-                            
-            <div class="w-full sm:w-1/2 md:w-1/3 mb-4 px-2">
-              <div class="relative bg-white rounded border">
-                <div class="p-4">
-                  <h3 class="text-lg font-bold">
-                    <a class="stretched-link" href="#" title="Card 2">
-                      Card 2
-                    </a>
-                  </h3>
-                  <time class="block mb-2 text-sm text-gray-600" datetime="2019-01-01">1st January 2019</time>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  </p>
-                </div>
-              </div>
-            </div>
+            {% endfor %}
+
           </div>
         </div>
           </div>
@@ -53,22 +42,22 @@ export const cardList = () => {
     },
     options: [
       {
-        name: 'loop through',
-        keyName: 'listName',
-        values: ['portal.solution_categories'],
-        type: 'dropdown'
+        name: 'Give your title',
+        keyName: 'title',
+        values: '',
+        type: 'text'
       },
       {
-        name: 'and show value',
-        keyName: 'listItemPath',
-        values: ['name', 'description'],
+        name: 'what do you wanna loop through',
+        keyName: 'listName',
+        values: ['portal.solution_categories'],
         type: 'dropdown'
       }
     ],
 
     selectedOptions: {
-      listName: 'portal.solution_categories',
-      listItemPath: 'name' // for articles use title
+      title: 'sample title',
+      listName: 'portal.solution_categories'
     }
   }
 };
